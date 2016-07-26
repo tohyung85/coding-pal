68623eac2e28521cb8b6ae1b2b6fc370338c44d5
Joshua Tan <tantohyung@gmail.com>
2016-07-26 13:23:33 +0000

    Hide flash messages if unrequired

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   context 'user enters webpage' do
+    render_views
     it 'should show landing page' do
       get :landing_page
       expect(response).to have_http_status(:success)
