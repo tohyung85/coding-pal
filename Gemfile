4b9e06afd1be18f94fd1621a71aaabc04e292d11
Joshua Tan <tantohyung@gmail.com>
2016-08-04 13:06:55 +0000

    Install carrierwave with uploading to aws

--- Gemfile
+++ Gemfile
@@ -37,6 +37,11 @@ gem 'devise', '>= 3.2.4'
 
 # Carrierwave gem
 gem 'carrierwave'
+# Carrierwave aws
+gem 'carrierwave-aws'
+
+# Figaro gem
+gem 'figaro'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731114922) do
+ActiveRecord::Schema.define(version: 20160804121628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160731114922) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "user_id"
+    t.string   "avatar"
   end
 
   create_table "users", force: :cascade do |t|
