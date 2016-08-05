66ed3dfd65f77a2ad7db60fd0ff5ddd39b0113c7
Joshua Tan <tantohyung@gmail.com>
2016-08-05 13:55:45 +0000

    Add user description to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804145424) do
+ActiveRecord::Schema.define(version: 20160805132820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,13 @@ ActiveRecord::Schema.define(version: 20160804145424) do
   add_index "groups", ["user_id"], name: "index_groups_on_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "user_name",  default: "my username"
-    t.boolean  "remote_ok?", default: true
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.string   "user_name",   default: "my username"
+    t.boolean  "remote_ok?",  default: true
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.integer  "user_id"
     t.string   "avatar"
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
