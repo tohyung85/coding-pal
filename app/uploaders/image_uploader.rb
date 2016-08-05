86d32bdcdea00b87fd973a6fb0dd25cc228a1fac
Joshua Tan <tantohyung@gmail.com>
2016-08-05 14:23:03 +0000

    Set group image uploader to higher resolution

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_fill: [300, 200]
+  process resize_to_fill: [600, 400]
 
   # Create different versions of your uploaded files:
   # version :thumb do
