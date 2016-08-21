d5975f7e6b71f99f6d08a4eb63429a6c5cdef58e
Joshua Tan <tantohyung@gmail.com>
2016-08-21 01:58:00 +0000

    Set up associations between user and user_messages model

--- app/models/user_message.rb
+++ app/models/user_message.rb
@@ -1,2 +1,4 @@
 class UserMessage < ActiveRecord::Base
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receipient, class_name: 'User'
 end
---
+++ spec/controllers/user_messages_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe UserMessagesController, type: :controller do
+
+end
