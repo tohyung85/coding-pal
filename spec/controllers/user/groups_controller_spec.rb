d04089e306ce4a4840dd40b9069a2e8412f9137a
Joshua Tan <tantohyung@gmail.com>
2016-08-25 12:22:40 +0000

    Rubocop fixes

---
+++ spec/controllers/user/groups_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User::GroupsController, type: :controller do
+  describe '#index' do
+    context 'user signed in' do
+      it 'should display page with user groups' do
+      end
+    end
+  end
+end
