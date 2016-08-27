704c54f69e2315e5802939e86eda436492988cb8
Joshua Tan <tantohyung@gmail.com>
2016-08-27 10:37:07 +0000

    Remove unnecessary alerts in bootstrap fix

--- app/assets/javascripts/bootstrap-ckeditor-fix.js
+++ app/assets/javascripts/bootstrap-ckeditor-fix.js
@@ -3,9 +3,7 @@
 //
 // Include this file AFTER both jQuery and bootstrap are loaded.
 $.fn.modal.Constructor.prototype.enforceFocus = function() {
-  alert('implementing fix');
   modal_this = this;
-  alert(modal_this);
   $(document).on('focusin.modal', function (e) {
     if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length 
     && $(e.target.parentNode).hasClass('cke_contents cke_reset')) {
