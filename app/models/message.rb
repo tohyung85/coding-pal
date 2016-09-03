39710f57fe858d5c1083937f72e34f0f20c4c539
Joshua Tan <tantohyung@gmail.com>
2016-09-03 06:49:37 +0000

    Set up group messages email

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,13 @@
 class Message < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+
+  after_create :send_group_message_notification_email
+
+  def send_group_message_notification_email
+    members = group.members
+    members.each do |member|
+      NotificationMailer.delay.notify_user_of_group_message(id, member.id)
+    end
+  end
 end
